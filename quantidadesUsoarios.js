1cb32052fe855aa66ed5d9f89af5e217fedf2dec
Florzi <106540640+Florzi@users.noreply.github.com>
2024-11-08 11:48:15 -0300

    Add files via upload

---
+++ quantidadesUsoarios.js
@@ -0,0 +1,20 @@
+async function quantidadeUsuarios() {
+    const url = "https://raw.githubusercontent.com/guilhermeonrails/api/ main/numero-usuarios.json"
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
+    console.log(dados)
+    const data = [
+        {
+        
+        x: nomeDasRedes,
+        y: quantidadeUsuarios,
+        type: 'bar'
+        
+        }
+        ]
+
+
+    }
+    quantidadeUsuarios()
